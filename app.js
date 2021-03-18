ff2e45de9c7ed40dc1c7642d6bb9c741aa427d65
Mathias Lopes Fernandes <mathiaslopesfernandes@outlook.fr>
2021-03-18 13:59:51 +0100

    Ajout d'accès à la page index + page non trouvé

--- app.js
+++ app.js
@@ -4,24 +4,26 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var pathPublic = __dirname + "/public";
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+//page d'accueil lorsque l'utilisateur s'est connecté
+app.get('/index', (req, res) => {
+    res.sendFile(path.join(pathPublic+'/pages/index.html'));
+});
 
+//route pour toutes les pages de webpages, pour quel ne soit pas accessible
+app.get('/pages/*', (req, res) => res.sendFile(path.join(pathPublic+'/pages/notfound.html')));
+
+//app.use(express.static(path.join(__dirname, 'public')));
+
+//laissé ces lignes après toutes les routes :  ----------------------------------------
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -35,7 +37,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.sendFile(path.join(pathPublic+'/pages/notfound.html'));
 });
 
 module.exports = app;
